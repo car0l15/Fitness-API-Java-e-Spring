8b3d7382519e5e24e2aacc8d5dab4fef8e794383
car0l15 <carol.nepomuceno95@gmail.com>
2024-03-06 11:22:50 -0300

    feat: implementando getWorkoutById

--- src/main/java/com/betrybe/fitness/controller/FitnessController.java
+++ src/main/java/com/betrybe/fitness/controller/FitnessController.java
@@ -1,6 +1,13 @@
 package com.betrybe.fitness.controller;
 
+import com.betrybe.fitness.dto.WorkoutDto;
 import com.betrybe.fitness.service.FitnessServiceInterface;
+import java.util.Objects;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +20,41 @@ public class FitnessController implements FitnessControllerInterface {
 
   private FitnessServiceInterface fitnessServiceInterface;
 
+  /**
+   * Instantiates a new Fitness controller.
+   *
+   * @param fitnessServiceInterface the fitness service interface
+   */
+  @Autowired
   public FitnessController(FitnessServiceInterface fitnessServiceInterface) {
     this.fitnessServiceInterface = fitnessServiceInterface;
   }
 
+  /**
+   * Gets welcome message.
+   *
+   * @return the welcome message
+   */
+  @GetMapping
+  public String getWelcomeMessage() {
+    return "Boas vindas à API de Fitness!";
+  }
+
+  /**
+   * Gets workout by id.
+   *
+   * @param id the id
+   * @return the workout by id
+   */
+  @GetMapping("workouts/{id}")
+  public ResponseEntity<Optional<WorkoutDto>> getWorkoutById(@PathVariable Long id) {
+
+    Optional<WorkoutDto> workoutDto = fitnessServiceInterface.getWorkout(id);
+
+    if (workoutDto.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(workoutDto);
+  }
 }
--- src/main/java/com/betrybe/fitness/service/FitnessService.java
+++ src/main/java/com/betrybe/fitness/service/FitnessService.java
@@ -3,6 +3,7 @@ package com.betrybe.fitness.service;
 import com.betrybe.fitness.database.FakeFitnessDatabase;
 import com.betrybe.fitness.dto.WorkoutCreationDto;
 import com.betrybe.fitness.dto.WorkoutDto;
+import com.betrybe.fitness.model.Workout;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,17 @@ public class FitnessService implements FitnessServiceInterface {
 
   @Override
   public Optional<WorkoutDto> getWorkout(Long id) {
-    return Optional.empty();
+    Optional<Workout> workout = fakeFitnessDatabase.getWorkout(id);
+
+    if (workout.isEmpty()) {
+      return Optional.empty();
+    }
+
+    WorkoutDto workoutDto = new WorkoutDto(workout.get().getId(), workout.get().getName(),
+        workout.get().getRepetitions());
+
+    return Optional.of(workoutDto);
+
   }
 
   @Override
